bf3a39c48ac81d141431a312bb40fb3265747b7f
Stas_Kozak <stas9239@gmail.com>
2020-07-24 20:04:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a streamr of neurofeedback matlab data to python format.
---
+++ src/hackathon_project/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__title__ = "Hachathon project"
+__description__ = "Streamr of neurofeedback data"
+__url__ = <GITHUB LINK>
+__doc__ = __description__ + " <" + __url__ + ">"
+__author__ = <YOUR NAME>
+__email__ = <YOUR EMAIL>
+__license__ = "MIT"
